a3a58035670ed0fb417761d38edb20403ff2fd0c
DenooVa <daniyel.safaee@gmail.com>
2019-12-09 10:19:52 +0330

    Change the code based on document

--- Shapes_recognition.py
+++ Shapes_recognition.py
@@ -1,5 +1,4 @@
-#%%
-# importing parts
+# importings
 import tensorflow as tf
 import keras
 from keras.models import Sequential
@@ -9,60 +8,40 @@ import os
 import numpy as np
 import matplotlib.pyplot as plt
 
-#%%
-# Data preparation
-train_image_generator = ImageDataGenerator(rescale=1./255) # Generator for our training data
-validation_image_generator = ImageDataGenerator(rescale=1./255) # Generator for our validation data
+# The 1./255 is to convert from uint8 to float32 in range [0,1].
+# load images
+Image_Generator = keras.preprocessing.image.ImageDataGenerator(rescale=1./255)
 
-#%%
-batch_size = 128
-epochs = 15
-IMG_HEIGHT = 150
-IMG_WIDTH = 150
-train_data_gen = train_image_generator.flow_from_directory(batch_size=batch_size,
-                                                           directory='/home/denova/Documents/py_projects/HW2/Shapes',
-                                                           shuffle=True,
-                                                           target_size=(IMG_HEIGHT, IMG_WIDTH),
-                                                           class_mode='categorical')
+BATCH_SIZE = 32
+IMG_HEIGHT = 224
+IMG_WIDTH = 224
 
-val_data_gen = validation_image_generator.flow_from_directory(batch_size=batch_size,
-                                                              directory='/home/denova/Documents/py_projects/HW2/Shapes',
-                                                              target_size=(IMG_HEIGHT, IMG_WIDTH),
-                                                              class_mode='categorical')
+train_data_gen = Image_Generator.flow_from_directory(
+    directory='/home/denova/Documents/py_projects/HW2/Shapes',
+    batch_size=BATCH_SIZE,
+    shuffle=True,
+    target_size=(IMG_HEIGHT, IMG_WIDTH),
+    class_mode='categorical')
 
-# %%
-sample_training_images, _ = next(train_data_gen)
+STEPS_PER_EPOCH = np.ceil(train_data_gen.n/BATCH_SIZE)
 
-# %%
-# This function will plot images in the form of a grid with 1 row and 5 columns where images are placed in each column.
-def plotImages(images_arr):
-    fig, axes = plt.subplots(1, 5, figsize=(20,20))
-    axes = axes.flatten()
-    for img, ax in zip( images_arr, axes):
-        ax.imshow(img)
-        ax.axis('off')
-    plt.tight_layout()
-    plt.show()
+# # showing a sample
+# def show_batch(image_batch, label_batch):
+#   plt.figure(figsize=(10,10))
+#   for n in range(25):
+#       ax = plt.subplot(5,5,n+1)
+#       plt.imshow(image_batch[n])
+#       plt.axis('off')
 
-plotImages(sample_training_images)
+# image_batch, label_batch = next(train_data_gen)
+# show_batch(image_batch, label_batch)
 
-#%%
-model = Sequential([
-    Conv2D(16, 3, padding='same', activation='relu', input_shape=(IMG_HEIGHT, IMG_WIDTH ,3)),
-    MaxPooling2D(),
-    Conv2D(32, 3, padding='same', activation='relu'),
-    MaxPooling2D(),
-    Conv2D(64, 3, padding='same', activation='relu'),
-    MaxPooling2D(),
-    Flatten(),
-    Dense(512, activation='relu'),
-    Dense(1, activation='softmax')
+model = keras.Sequential([
+    keras.layers.Flatten(input_shape=(28, 28)),
+    keras.layers.Dense(128, activation='relu'),
+    keras.layers.Dense(10, activation='softmax')
 ])
 
-# %%
 model.compile(optimizer='adam',
-              loss='binary_crossentropy',
+              loss='sparse_categorical_crossentropy',
               metrics=['accuracy'])
-
-# %%
-model.summary()
