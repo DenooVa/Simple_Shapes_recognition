8d4acd10fb338e5dda6ebf39ed00c06044f495b4
DenooVa <daniyel.safaee@gmail.com>
2019-12-10 21:08:00 +0330

    try to increase accuracy rate

--- Shapes_recognition.py
+++ Shapes_recognition.py
@@ -7,20 +7,28 @@ import keras
 import matplotlib.pyplot as plt
 import numpy as np
 # The 1./255 is to convert from uint8 to float32 in range [0,1].
-image_generator = ImageDataGenerator(rescale=1./255)
+image_generator = ImageDataGenerator(
+    rescale=1./255,
+    rotation_range=45,
+    # width_shift_range=.15,
+    # height_shift_range=.15,
+    vertical_flip=True
+    )
 train_data_gen = image_generator.flow_from_directory(
     directory='/home/denova/Documents/py_projects/HW2/train',
     target_size=(28,28),
-    batch_size=10,
+    batch_size=300,
     color_mode='rgb'
 )
 model = keras.Sequential([
     Conv2D(16, 3, padding='same', activation='relu', input_shape=(28,28,3)),
     MaxPooling2D(),
+    Dropout(0.2),
     Conv2D(32, 3, padding='same', activation='relu'),
     MaxPooling2D(),
     Conv2D(64, 3, padding='same', activation='relu'),
     MaxPooling2D(),
+    Dropout(0,2),
     keras.layers.Flatten(),
     keras.layers.Dense(512, activation='relu'), # MLP part of the CNN
     keras.layers.Dense(3, activation='softmax')
@@ -30,10 +38,11 @@ model.compile(optimizer='rmsprop',
               loss='categorical_crossentropy',
               metrics=['accuracy'])
 model.summary()
+class_names = list(train_data_gen.class_indices.keys())
 history = model.fit_generator(
     train_data_gen,
     steps_per_epoch= train_data_gen.n // 10,
-    epochs= 10
+    epochs= 40
 )
 test = image_generator.flow_from_directory(
     directory='/home/denova/Documents/py_projects/HW2/test',
@@ -47,3 +56,17 @@ elif np.argmax(prediction[0]) == 1:
     print('square')
 else:
     print('triangle')
+# visualize
+acc = history.history['accuracy']
+loss = history.history['loss']
+epochs_range = range(40)
+plt.figure(figsize=(8, 8))
+plt.subplot(1, 2, 1)
+plt.plot(epochs_range, acc, label='Training Accuracy')
+plt.legend(loc='lower right')
+plt.title('Training Accuracy')
+plt.subplot(1, 2, 2)
+plt.plot(epochs_range, loss, label='Training Loss')
+plt.legend(loc='upper right')
+plt.title('Training Loss')
+plt.show()
